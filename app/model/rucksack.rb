494513018aa3508224ca4b817753f7e6aead8a02
Jon Kern <jonkernpa@gmail.com>
2022-12-03 13:51:41 -0500

    Day 3: Rucksack contents Part 2

--- app/model/rucksack.rb
+++ app/model/rucksack.rb
@@ -1,31 +1,48 @@
 class Rucksack
 
+  GROUP_SIZE = 3
+
   attr_accessor :compartments, :repeated, :priority
   class << self
-    attr_accessor :priority_sum
+    attr_accessor :priority_sum, :groups, :group_count, :badge_sum
   end
 
   @priority_sum = nil
+  @groups = [[]]
+  @group_count = nil
+  @badge_sum = nil
 
   def initialize(contents)
     Rucksack.priority_sum ||= 0
+    Rucksack.group_count ||= 0
+    # Rucksack.badge_sum ||= 0
 
     @compartments = []
     @compartments = contents.chars.each_slice( (contents.size/2).round ).to_a
-    # puts @compartments[0].join('')
-    # puts @compartments[1].join('')
+
     @repeated = (@compartments[0] & @compartments[1]).join('')
-    @priority = prioritize(@repeated)
-    # puts "#{@repeated} - #{@priority}"
+
+    @priority = Rucksack.prioritize(@repeated)
     Rucksack.priority_sum += @priority
-    # puts "Sum: #{Rucksack.priority_sum}"
+
+    if Rucksack.groups.last.size < GROUP_SIZE
+      Rucksack.groups.last << self
+      Rucksack.group_count += 1
+    else
+      Rucksack.group_count = 0
+      Rucksack.groups << [self]
+    end
   end
 
-  def prioritize(item)
+  def to_s
+    "%s %d" % [@repeated, @priority]
+  end
+
+  def self.prioritize(item)
     ranking = %w(- a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z )
-    index = ranking.index(item)
+    index = ranking.index(item.strip)
     unless index
-      puts "!!! Ranking #{item} --> #{index.inspect}"
+      puts "   !!! FAILED to rank '#{item}' --> #{index.inspect}"
     end
     index
   end
@@ -36,9 +53,43 @@ class Rucksack
 
   def self.process(file_name)
     # Rucksack.reset_priority_sum
+    lines = 0
     File.readlines("#{file_name}").each do |contents|
-      Rucksack.new(contents)
+      Rucksack.new(contents.strip)
+      lines += 1
     end
+    puts "#{lines} lines processed, #{Rucksack.groups.size} groups"
+  end
+
+  def self.reset_group
+    @group_count = 0
+  end
+
+  def self.wipe_groups
+    @groups = [[]]
+  end
+
+  def self.badge(group)
+    if group.size == 3
+      common_1 = group[0].compartments.flatten & group[1].compartments.flatten
+      common_2 = group[1].compartments.flatten & group[2].compartments.flatten
+      (common_1 & common_2).join('')
+    else
+      '-'
+    end
+  end
+
+  def self.reset_badge_sum
+    Rucksack.badge_sum = 0
+  end
+
+  def self.total_badges
+    Rucksack.badge_sum = 0
+    Rucksack.groups.each do |group|
+      rucksack_badge = Rucksack.badge(group)
+      Rucksack.badge_sum += Rucksack.prioritize(rucksack_badge)
+    end
+    Rucksack.badge_sum
   end
 
 end
--- spec/day3_spec.rb
+++ spec/day3_spec.rb
@@ -8,6 +8,12 @@ require 'spec_helper'
 
 RSpec.describe 'DAY 3: Rucksack Contents', type: :model do
 
+  before :each do
+    Rucksack.wipe_groups
+    Rucksack.reset_priority_sum
+
+  end
+
   it 'has contents in 2 compartments' do
     rucksack = Rucksack.new('vJrwpWtwJgWrhcsFMMfFFhFp')
     expect(rucksack.compartments.size).to eql(2)
@@ -26,12 +32,10 @@ RSpec.describe 'DAY 3: Rucksack Contents', type: :model do
   end
 
   it 'can reset priority_sum' do
-    Rucksack.reset_priority_sum
     expect(Rucksack.priority_sum).to eql(0)
   end
 
   it 'accumulates priorities' do
-    Rucksack.reset_priority_sum
     Rucksack.new('vJrwpWtwJgWrhcsFMMfFFhFp')
     Rucksack.new('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL')
     Rucksack.new('PmmdzqPrVvPwwTWBwg')
@@ -43,14 +47,83 @@ RSpec.describe 'DAY 3: Rucksack Contents', type: :model do
   end
 
   it 'processes contents from a file' do
-    Rucksack.reset_priority_sum
     Rucksack.process('spec/test_input_day3.txt')
     expect(Rucksack.priority_sum).to eql(157)
   end
 
   it 'does the day 3 exercise' do
-    Rucksack.reset_priority_sum
     Rucksack.process('app/data/day3.txt')
     ap Rucksack.priority_sum
   end
+
+  it 'defines group as cluster of 3' do
+    expect(Rucksack::GROUP_SIZE).to eql(3)
+  end
+
+  it 'resets the group counter' do
+    Rucksack.reset_group
+    expect(Rucksack.group_count).to eql(0)
+  end
+
+  it 'increments group count on object creation' do
+    Rucksack.reset_group
+    Rucksack.new('vJrwpWtwJgWrhcsFMMfFFhFp')
+    expect(Rucksack.group_count).to eql(1)
+  end
+
+  it 'has groups of elves' do
+    Rucksack.new('vJrwpWtwJgWrhcsFMMfFFhFp')
+    Rucksack.new('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL')
+    Rucksack.new('PmmdzqPrVvPwwTWBwg')
+
+    Rucksack.new('wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn')
+    Rucksack.new('ttgJtRGJQctTZtZT')
+    Rucksack.new('CrZsJsPPZsGzwwsLwLmpwMDw')
+
+    Rucksack.new('GGVGlqWFgVfFqqVZGFlblJPMsDbbMrDMpDsJRn')
+    Rucksack.new('LwzHtwdLHHwDrzPZzzsJbJ')
+    Rucksack.new('wdLTBvSvHvZVGCjhfN')
+
+    expect(Rucksack.groups.size).to eql(3)
+  end
+
+  it 'detects group badge' do
+    Rucksack.wipe_groups
+
+    Rucksack.new('vJrwpWtwJgWrhcsFMMfFFhFp')
+    Rucksack.new('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL')
+    Rucksack.new('PmmdzqPrVvPwwTWBwg')
+
+    expect(Rucksack.badge(Rucksack.groups[0])).to eql('r')
+  end
+
+  it 'detects badges for multiple groups and sums the priority' do
+    # Rucksack.reset_badge_sum
+
+    Rucksack.new('vJrwpWtwJgWrhcsFMMfFFhFp')
+    Rucksack.new('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL')
+    Rucksack.new('PmmdzqPrVvPwwTWBwg')
+
+    Rucksack.new('wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn')
+    Rucksack.new('ttgJtRGJQctTZtZT')
+    Rucksack.new('CrZsJsPPZsGzwwsLwLmpwMDw')
+
+    Rucksack.total_badges
+    expect(Rucksack.badge_sum).to eql(70)
+  end
+
+  it 'does the day 3 exercise test' do
+    Rucksack.process('spec/test_input_day3.txt')
+    expect(Rucksack.groups.size).to eql(2)
+    ap Rucksack.total_badges
+  end
+
+  it 'does the day 3 exercise, part 2' do
+    Rucksack.process('app/data/day3.txt')
+
+    total = Rucksack.total_badges
+    puts "Total Badges: #{total}"
+    expect(total).to eql(2821)
+  end
+
 end
