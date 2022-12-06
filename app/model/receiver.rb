d877ac4765be6e9421c52697e392c54be79ab0c8
Jon Kern <jonkernpa@gmail.com>
2022-12-06 18:10:45 -0500

    Day 6: refactored: improved some names... Added helpful comments

--- app/model/receiver.rb
+++ app/model/receiver.rb
@@ -17,27 +17,40 @@ class Receiver
   end
 
   def find_packet_marker(str)
-    @packet_marker, @packet_start = find_repeater(str, 4)
+    @packet_marker, @packet_start = find_unique(str, 4)
   end
 
   def find_message_start(str)
-    @message_marker, @message_start = find_repeater(str, 14)
+    @message_marker, @message_start = find_unique(str, 14)
   end
 
-  def find_repeater(str, num_distinct = 4)
+  # Look for the first occurrence of a string of unique characters of the specified length
+  def find_unique(str, desired_length)
+    # Given a long slew of characters
     chars = str.chars
-    processed = chars.slice!(0..(num_distinct-2))
+
+    # pre-load the first few characters, and take them out of the list
+    # This list will remain at the "desired_length" in size
+    processed = chars.slice!(0..(desired_length-2))
+
+    # Use a Set as it removes duplicates chars (one of our rules)
     set = Set.new(processed)
+
+    # Iterate through the rest of the list, one character at a time
     chars.each_with_index do |c,i|
+      # Add it to the list & the set
       processed << c
       set << c
-      if set.size == processed.size && set.size == num_distinct
-        # found packet!
-        repeat_start = i + num_distinct
+
+      # Check if the current list and the set are identical
+      # --> implying a unique list has been detected
+      if set.size == processed.size && set.size == desired_length
+        repeat_start = i + desired_length
         puts "Found after #{repeat_start} character"
         repeat_marker = processed.join
         return repeat_marker, repeat_start
       else
+        # Otherwise, remove the top of the list and start the set over
         processed.slice!(0)
         set = Set.new(processed)
       end
